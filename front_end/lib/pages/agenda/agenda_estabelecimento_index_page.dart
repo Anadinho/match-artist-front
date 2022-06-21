308bd4e36c8a859aa5829d964479371b0779f5bd
Tallyson Bruno Candido Barbosa <tallysondesign@gmail.com>
2022-06-20 21:46:20 -0300

    feat: atualização de listagem de solicitações

--- front_end/lib/pages/agenda/agenda_estabelecimento_index_page.dart
+++ front_end/lib/pages/agenda/agenda_estabelecimento_index_page.dart
@@ -47,41 +47,59 @@ class _AgendaEstabelecimentoPageState extends State<AgendaEstabelecimentoPage> {
               child: ListView.separated(
                 shrinkWrap: true,
                 itemCount: list.length,
-                itemBuilder: (_, idx) => Slidable(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      color: Colors.grey[200],
-                    ),
-                    child: ListTile(
-                      // leading: Text(list[idx].id.toString()),
-                      // trailing: Icon(Icons.arrow_forward),
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Container(
-                            color: Colors.blue[900],
-                            child: Text(list[idx].artista,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w600, fontSize: 16)),
-                          ),
-                          Text(list[idx].id.toString(),
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w600, fontSize: 16)),
-                          Text(list[idx].is_artista,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w600, fontSize: 12)),
-                          Text(list[idx].evento,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w600, fontSize: 14)),
-                        ],
+                itemBuilder: (_, idx) => ListTile(
+                  contentPadding: EdgeInsets.all(0),
+                  // leading: Text(list[idx].id.toString()),
+                  // trailing: Icon(Icons.arrow_forward),
+                  title: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(10),
+                                topLeft: Radius.circular(10)),
+                            color: Color.fromARGB(255, 0, 3, 60)),
+                        padding: EdgeInsets.all(10),
+                        child: Text(list[idx].artista,
+                            style: TextStyle(
+                                color: Colors.grey[300], fontSize: 16)),
                       ),
-                      // onTap: () => Navigator.of(context)
-                      //     .pushNamed('/subIndexArtista', arguments: list[idx]),
-                    ),
+                      Container(
+                        padding: EdgeInsets.only(top: 4, left: 8),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                        ),
+                        child: Text(list[idx].evento.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 16)),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 4, left: 8),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                        ),
+                        child: Text(list[idx].is_artista,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 12)),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 4, left: 8, bottom: 8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)),
+                          color: Colors.grey[200],
+                        ),
+                        child: Text(list[idx].evento,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 14)),
+                      ),
+                    ],
                   ),
-                  actionExtentRatio: 0.25,
-                  actionPane: const SlidableDrawerActionPane(),
+                  // onTap: () => Navigator.of(context)
+                  //     .pushNamed('/subIndexArtista', arguments: list[idx]),
                 ),
                 separatorBuilder: (BuildContext context, int index) =>
                     Padding(padding: EdgeInsets.all(8)),
